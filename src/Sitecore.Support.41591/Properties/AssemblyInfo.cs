53201d5656ad4ebbdb8d88d05c56d0780f281f8f
MasoudA <masoud_nitro@yahoo.com>
2017-08-25 17:38:03 +0800

    ListItemsEditor.xaml.xml is taken from WFFM 8.0 Update-3

---
+++ src/Sitecore.Support.41591/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.41591")]
+[assembly: AssemblyProduct("Sitecore.Support.41591")]
+[assembly: ComVisible(false)]
